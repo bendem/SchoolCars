1a94ff407b4395534622a3e96c4b44f404f8518e
bendem <online@bendem.be>
2014-12-18 15:23:25 +0100

    Added a Comparable interface for easier to maintain object comparisons

---
+++ header/utils/Comparable.hpp
@@ -0,0 +1,19 @@
+#ifndef COMPARABLE_HPP
+#define COMPARABLE_HPP
+
+template<class T>
+class Comparable {
+
+public:
+    virtual int compareTo(const T&) const = 0;
+    
+    bool operator==(const T& param) const { return this->compareTo(param) == 0; }
+    bool operator!=(const T& param) const { return this->compareTo(param) != 0; }
+    bool operator> (const T& param) const { return this->compareTo(param) >  0; }
+    bool operator< (const T& param) const { return this->compareTo(param) <  0; }
+    bool operator>=(const T& param) const { return this->compareTo(param) >= 0; }
+    bool operator<=(const T& param) const { return this->compareTo(param) <= 0; }
+
+};
+
+#endif
